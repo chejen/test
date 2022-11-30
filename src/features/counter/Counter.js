13c149090c0d0d7b9235912789dd8822d29195d3
chejen <jkopre.qek@gmail.com>
2022-11-30 19:02:45 +0800

    tmp

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { createSelector } from 'reselect';
+import { createCachedSelector } from 're-reselect';
 import {
   decrement,
   increment,
+  resetTo0,
   incrementByAmount,
   incrementAsync,
   incrementIfOdd,
@@ -10,12 +13,38 @@ import {
 } from './counterSlice';
 import styles from './Counter.module.css';
 
+const selectResult = createSelector(selectCount, (count) => {
+  console.log('[debug] selectResult');
+  return count * 2;
+});
+
+const selectCachedResult = createCachedSelector(
+  // inputSelectors
+  selectCount,
+
+  // resultFunc
+  (count) => {
+    console.log('[debug] selectCachedResult');
+    return count * 2;
+  },
+)(
+  // re-reselect keySelector (receives selectors' arguments)
+  // Use "libraryName" as cacheKey
+  (_state_, count) => {
+    console.log('@@@', { count });
+    return count;
+  }
+);
+
 export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
 
   const incrementValue = Number(incrementAmount) || 0;
+
+  // const res = useSelector(selectResult);
+  const res = selectCachedResult(count);
 
   return (
     <div>
@@ -28,6 +57,8 @@ export function Counter() {
           -
         </button>
         <span className={styles.value}>{count}</span>
+        {' | '}
+        <span className={styles.value}>{res}</span>
         <button
           className={styles.button}
           aria-label="Increment value"
@@ -60,6 +91,12 @@ export function Counter() {
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
           Add If Odd
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => dispatch(resetTo0())}
+        >
+          Reset to 0
         </button>
       </div>
     </div>
--- src/features/counter/counterSlice.spec.js
+++ src/features/counter/counterSlice.spec.js
@@ -2,32 +2,97 @@ import counterReducer, {
   increment,
   decrement,
   incrementByAmount,
+  selectResult,
+  selectResultFromAry,
+  selectResultAry,
+  selectCachedResult,
 } from './counterSlice';
 
 describe('counter reducer', () => {
   const initialState = {
     value: 3,
     status: 'idle',
-  };
-  it('should handle initial state', () => {
-    expect(counterReducer(undefined, { type: 'unknown' })).toEqual({
+    counter: {
       value: 0,
-      status: 'idle',
-    });
+    },
+  };
+  // it('should handle initial state', () => {
+  //   expect(counterReducer(undefined, { type: 'unknown' })).toEqual({
+  //     value: 0,
+  //     status: 'idle',
+  //     counter: {
+  //       value: 0,
+  //     },
+  //   });
+  // });
+
+  // it('should handle increment', () => {
+  //   const actual = counterReducer(initialState, increment());
+  //   expect(actual.value).toEqual(4);
+  // });
+
+  
+  it('test1-selectResult-premitive', () => {
+    let result = selectResult(initialState, 5);
+    expect(result).toEqual(5);
+    result = selectResult(initialState, 5);
+    expect(result).toEqual(5);
+    console.log('----------');
+  });
+  it('test2-selectResult-premitive-interleaving', () => {
+    let result = selectResult(initialState, 3);
+    expect(result).toEqual(3);
+    result = selectResult(initialState, 4);
+    expect(result).toEqual(4);
+    result = selectResult(initialState, 3);
+    expect(result).toEqual(3);
+    console.log('----------');
+  });
+  it('test3-selectResultFromAry-reference-same', () => {
+    const arr = [5];
+    let result = selectResultFromAry(initialState, arr);
+    expect(result).toEqual(5);
+    result = selectResultFromAry(initialState, arr);
+    expect(result).toEqual(5);
+    console.log('----------');
+  });
+  it('test4-selectResultFromAry-reference-diff', () => {
+    let result = selectResultFromAry(initialState, [4]);
+    expect(result).toEqual(4);
+    result = selectResultFromAry(initialState, [4]);
+    expect(result).toEqual(4);
+    console.log('-----4-----');
+  });
+  it('test5-selectResultAry-reference-same', () => {
+    const arr = [7];
+    let result = selectResultAry(initialState, arr);
+    expect(result).toEqual(7);
+    result = selectResultAry(initialState, arr);
+    expect(result).toEqual(7);
+    console.log('-----5-----');
+  });
+  it('test6-selectResultAry-reference-diff', () => {
+    let result = selectResultAry(initialState, [8]);
+    expect(result).toEqual(8);
+    result = selectResultAry(initialState, [8]);
+    expect(result).toEqual(8);
+    console.log('-----6-----');
+  });
+  it('test7-what-if-state-changed', () => {
+    let result = selectResult(initialState, 2);
+    expect(result).toEqual(2);
+    result = selectResult({...initialState}, 2);
+    expect(result).toEqual(2);
+    console.log('-----7-----');
   });
 
-  it('should handle increment', () => {
-    const actual = counterReducer(initialState, increment());
-    expect(actual.value).toEqual(4);
-  });
+  // it('should handle decrement', () => {
+  //   const actual = counterReducer(initialState, decrement());
+  //   expect(actual.value).toEqual(2);
+  // });
 
-  it('should handle decrement', () => {
-    const actual = counterReducer(initialState, decrement());
-    expect(actual.value).toEqual(2);
-  });
-
-  it('should handle incrementByAmount', () => {
-    const actual = counterReducer(initialState, incrementByAmount(2));
-    expect(actual.value).toEqual(5);
-  });
+  // it('should handle incrementByAmount', () => {
+  //   const actual = counterReducer(initialState, incrementByAmount(2));
+  //   expect(actual.value).toEqual(5);
+  // });
 });
